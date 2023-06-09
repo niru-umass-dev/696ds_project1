bbc2cea78d84fd34b7048256ac0dc280990ff9fb
Philip George <pgeorge@umass.edu>
2023-06-08 21:57:02 -0400

    Changed negation dataset creation code and created complete negation dataset

--- src/create_negate_json.py
+++ src/create_negate_json.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 import json
 
-df = pd.read_excel('data/negation_annotation.xlsx', index_col=0)
+# df = pd.read_excel('data/negation_annotation.xlsx', index_col=0)
+df = pd.read_csv('data/negation_annotation.csv', index_col=0)
 original = json.load(open('data/combined_data_base_split_complete.json', 'r'))
 
-for i in range(20,30):
-
+for i in range(0, 48):
     example = df.loc[(df['example_no'] == i) & (df['summ_type'] == 'refs_a')]
     neg_list = example['negated_simple_sent'].tolist()
 
@@ -14,7 +14,6 @@ for i in range(20,30):
     neg_list = series.fillna('').tolist()
 
     original[i]['refs_a_neg'] = " ".join(neg_list)
-    
-    
+
 with open('data/combined_data_negation_split_complete.json', 'w') as f:
-    json.dump(original[20:30], f)
+    json.dump(original, f)
